bf391584973625e8a1306f765aa9361e45f94811
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 01:42:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from aiogram.types import (
     InputMediaPhoto
 )
 
-from config import BOT_TOKEN, CHANNEL_ID, ADMIN_ID
+from config import BOT_TOKEN, ADMIN_ID
 from ai import generate_text
 from media import generate_images
-from db import init_db, create_user, get_user
+from db import init_db, create_user
 
 
 # ================= FASTAPI =================
@@ -45,13 +45,18 @@ menu = ReplyKeyboardMarkup(
 @dp.message(lambda m: m.text == "/start")
 async def start(m: types.Message):
     await create_user(m.from_user.id)
-    await m.answer("🚀 V5 SaaS ready", reply_markup=menu)
+
+    await m.answer(
+        "🚀 V5 SaaS ready",
+        reply_markup=menu
+    )
 
 
 # ================= AI POST =================
 @dp.message(lambda m: m.text == "🔥 AI Post")
 async def ai_post(m: types.Message):
     text, topic = await generate_text()
+
     await m.answer(text)
 
 
@@ -103,6 +108,12 @@ async def admin(m: types.Message):
 async def startup():
     await init_db()
 
+    # удаляем webhook
+    await bot.delete_webhook(
+        drop_pending_updates=True
+    )
+
+    # запускаем polling
     asyncio.create_task(
         dp.start_polling(bot)
     )
