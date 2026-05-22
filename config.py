1fcb72423d875ffc6ff43354ba31ac2c8ae4d1ba
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 01:20:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # например "-100123"
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
