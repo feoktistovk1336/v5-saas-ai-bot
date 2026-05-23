81d6f8eab4f69e57a38a6a6887a3601a4c19762e
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 22:22:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import BOT_TOKEN
+from database.db import init_db
 
 from handlers.start import router as start_router
 from handlers.content import router as content_router
@@ -10,21 +13,16 @@ from handlers.rewrite import router as rewrite_router
 from handlers.admin import router as admin_router
 from handlers.payments import router as payments_router
 
-from database.db import init_db
-
 
 bot = Bot(
     token=BOT_TOKEN,
-    default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML
-    )
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 
 dp = Dispatcher()
 
 
-async def startup():
-
+async def main():
     await init_db()
 
     dp.include_router(start_router)
@@ -33,22 +31,12 @@ async def startup():
     dp.include_router(admin_router)
     dp.include_router(payments_router)
 
+    await bot.delete_webhook(drop_pending_updates=True)
+
     print("🚀 PRIMEONIX AI STARTED")
-
-
-async def main():
-
-    await startup()
-
-    await bot.delete_webhook(
-        drop_pending_updates=True
-    )
 
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-
-    import asyncio
-
     asyncio.run(main())
