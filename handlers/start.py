4e62f5c1d01df59f41311141ab3cc9e961513ac0
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 22:24:56 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,6 @@
-from aiogram import Router
-from aiogram.types import (
-    ReplyKeyboardMarkup,
-    KeyboardButton
-)
+from aiogram import Router, types
 from aiogram.filters import CommandStart
-from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 from database.db import create_user
 
@@ -53,28 +49,25 @@ content_menu = ReplyKeyboardMarkup(
 
 @router.message(CommandStart())
 async def start(m: types.Message):
-
     await create_user(m.from_user.id)
 
     await m.answer(
         "🚀 <b>PrimeOnix AI</b>\n\n"
-        "AI SaaS платформа для генерации контента.",
+        "AI SaaS платформа для постов, каруселей, Reels и контент-планов.",
         reply_markup=main_menu
     )
 
 
 @router.message(lambda m: m.text == "📦 Контент")
 async def open_content(m: types.Message):
-
     await m.answer(
-        "📦 Раздел контента",
+        "📦 <b>Раздел контента</b>",
         reply_markup=content_menu
     )
 
 
 @router.message(lambda m: m.text == "⬅️ Назад")
-async def back_menu(m: types.Message):
-
+async def back(m: types.Message):
     await m.answer(
         "🏠 Главное меню",
         reply_markup=main_menu
