54cf3c58af7bf777352ceec2aa3844c54339353f
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 17:08:03 +0300

    Update media.py

--- media.py
+++ media.py
@@ -1,27 +1,17 @@
 import random
 
 
-AI_IMAGES = [
-
-    "https://images.unsplash.com/photo-1677442136019-21780ecad995?w=1080",
-    "https://images.unsplash.com/photo-1674027392884-7515e76d7d24?w=1080",
-    "https://images.unsplash.com/photo-1676299081847-824916de030a?w=1080",
-    "https://images.unsplash.com/photo-1518770660439-4636190af475?w=1080",
-    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=1080",
-    "https://images.unsplash.com/photo-1552664730-d307ca884978?w=1080",
-    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=1080",
-    "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=1080",
-    "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=1080",
-    "https://images.unsplash.com/photo-1535223289827-42f1e9919769?w=1080"
-
-]
-
-
 async def generate_images(topic, count=5):
-    images = random.sample(
-        AI_IMAGES,
-        min(count, len(AI_IMAGES))
-    )
+    images = []
+
+    for _ in range(count):
+        seed = random.randint(1, 99999999)
+
+        url = (
+            f"https://picsum.photos/seed/{seed}/1080/1080"
+        )
+
+        images.append(url)
 
     return images
 
