39df8e02842745dada71dac4d32f5d599c84c914
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 16:17:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -142,8 +142,8 @@ async def create_ai_image(image_url, title):
 
         draw = ImageDraw.Draw(image)
 
-        # ===== FONT =====
-       try:
+        # font
+try:
 
     font = ImageFont.truetype(
         "arial.ttf",
