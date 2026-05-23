2ec61516b7c16ba16de7d2023161acd36dc250f5
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 22:23:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,14 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN", "")
 
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 
-BOT_USERNAME = os.getenv("BOT_USERNAME", "primeonixbot")
-
 BRAND_USERNAME = "@primeonix26"
-
-DB_PATH = "database.db"
 
 FREE_LIMIT = 5
 PRO_PRICE_STARS = 199
 PRO_DAYS = 30
-
-REPLICATE_API_TOKEN = "r8_9ytQuNda8ki6mfmz066ikudBezV2L3a37xiBe"
+DB_PATH = "database.sqlite"
