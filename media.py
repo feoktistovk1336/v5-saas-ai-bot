9b48c3c0ea794964b907ee4f2dd16fc5d7cc633c
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-23 19:35:09 +0300

    Update media.py

--- media.py
+++ media.py
@@ -6,14 +6,14 @@ AI_BACKGROUNDS = [
     "https://images.unsplash.com/photo-1518770660439-4636190af475?w=1080&q=95",
     "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=1080&q=95",
     "https://images.unsplash.com/photo-1535223289827-42f1e9919769?w=1080&q=95",
-    "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1080&q=95",
-    "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=1080&q=95",
     "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=1080&q=95",
-    "https://images.unsplash.com/photo-1557683316-973673baf926?w=1080&q=95",
     "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=1080&q=95",
-    "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=1080&q=95",
+    "https://images.unsplash.com/photo-1550439062-609e1531270e?w=1080&q=95",
+    "https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1080&q=95",
+    "https://images.unsplash.com/photo-1557683316-973673baf926?w=1080&q=95",
     "https://images.unsplash.com/photo-1557682250-33bd709cbe85?w=1080&q=95",
-    "https://images.unsplash.com/photo-1550439062-609e1531270e?w=1080&q=95"
+    "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=1080&q=95",
+    "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=1080&q=95"
 ]
 
 
